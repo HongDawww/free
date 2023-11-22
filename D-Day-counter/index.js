506850b9ff507fedf656033b4a8f20532e18fe72
HongDawww <d939305@naver.com>
2023-11-22 20:35:44 +0900

    과거시간 측정 불가

--- D-Day-counter/index.js
+++ D-Day-counter/index.js
@@ -1,6 +1,6 @@
 const messageContainer = document.querySelector("#d-day-message");
-const conatiner = document.querySelector("#d-day-container");
-// conatiner.style.display = 'none';
+const container = document.querySelector("#d-day-container");
+container.style.display = 'none';
 messageContainer.innerHTML = '<h3>D-day를 입력해주세요</h3>'
 
 const dateFormMaker = function () {
@@ -12,31 +12,42 @@ const dateFormMaker = function () {
 	return dateFormat;
   };
 
-  const countMaker = function () {
-	const targetDateInput = dateFormMaker();
-	const nowDate = new Date();
-	const targetDate = new Date(targetDateInput).setHours(0, 0, 0, 0);
-	const remainig = (targetDate - nowDate) / 1000;
-	
-	if(remainig <= 0) {
-		messageContainer.innerHTML = '<h3>측정 불가 !! <br> 과거 입니다</h3>'
-	} else if (isNaN(remainig)){
-		messageContainer.innerHTML = '<h3>유효한 시간대가 아닙니다!!</h3>'
-	}
+  const counterMaker = function () {
+		const targetDateInput = dateFormMaker();
+		const nowDate = new Date();
+		const targetDate = new Date(targetDateInput).setHours(0, 0, 0, 0);
+		const remainig = (targetDate - nowDate) / 1000;
+		
+		if(remainig <= 0) {
+			container.style.display = 'none';
+			messageContainer.innerHTML = '<h3>측정 불가 !! <br> 과거 입니다</h3>'
+			messageContainer.style.display = 'flex';
+		} else if (isNaN(remainig)){
+			container.style.display = 'none';
+			messageContainer.innerHTML = '<h3>유효한 시간대가 아닙니다!!</h3>'
+			messageContainer.style.display = 'flex';
+			return;
+		}
 
-	const remainigObj = {
-		remainigDate: Math.floor(remainig / 3600 / 24),
-		remainigHours: Math.floor(remainig / 3600) % 24,
-		remainigMin: Math.floor(remainig / 60) % 60,
-		remainigSec: Math.floor(remainig) % 60
-	}
-	
-	const documentArr = ['days','hours','min','sec']
-	const timeKeys = Object.keys(remainigObj);
+		const remainigObj = {
+			remainigDate: Math.floor(remainig / 3600 / 24),
+			remainigHours: Math.floor(remainig / 3600) % 24,
+			remainigMin: Math.floor(remainig / 60) % 60,
+			remainigSec: Math.floor(remainig) % 60
+		}
+		
+		const documentArr = ['days','hours','min','sec']
+		const timeKeys = Object.keys(remainigObj);
+		
+		let i = 0;
+		for (let tag of documentArr) {
+			document.getElementById(tag).textContent = remainigObj[timeKeys[i]];
+			i++;
+		}
+	};
 
-	let i = 0;
-	for (let tag of documentArr) {
-		document.getElementById(tag).textContent = remainigObj[timeKeys[i]];
-		i++;
+	const starter = function() {
+		container.style.display = 'flex';
+		messageContainer.style.display = 'none';
+		counterMaker();
 	}
- };
--- D-Day-counter/script-for-of.js
+++ D-Day-counter/script-for-of.js
@@ -40,4 +40,4 @@ const dateFormMaker = function () {
 		document.getElementById(tag).textContent = remainigObj[timeKeys[i]];
 		i++;
 	}
- };
+ }; 
