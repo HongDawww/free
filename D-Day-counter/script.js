563fe3aabcdcc3216b2983965e68ca75a5a86542
HongDawww <d939305@naver.com>
2023-11-22 19:44:27 +0900

    for 문 js

---
+++ D-Day-counter/script.js
@@ -0,0 +1,55 @@
+const messageContainer = document.querySelector("#d-day-message");
+const conatiner = document.querySelector("#d-day-container");
+// conatiner.style.display = 'none';
+messageContainer.innerHTML = '<h3>D-day를 입력해주세요</h3>'
+
+const dateFormMaker = function () {
+	const inputYear = document.querySelector("#target-year-input").value;
+	const inputMonth = document.querySelector("#target-month-input").value;
+	const inputDate = document.querySelector("#target-date-input").value;
+
+	const dateFormat = `${inputYear}-${inputMonth}-${inputDate}`;
+	return dateFormat;
+  };
+
+  const countMaker = function () {
+	const targetDateInput = dateFormMaker();
+	const nowDate = new Date();
+	const targetDate = new Date(targetDateInput).setHours(0, 0, 0, 0);
+	const remainig = (targetDate - nowDate) / 1000;
+	
+	if(remainig <= 0) {
+		messageContainer.innerHTML = '<h3>측정 불가 !! <br> 과거 입니다</h3>'
+	} else if (isNaN(remainig)){
+		messageContainer.innerHTML = '<h3>유효한 시간대가 아닙니다!!</h3>'
+	}
+
+	const remainigObj = {
+		remainigDate: Math.floor(remainig / 3600 / 24),
+		remainigHours: Math.floor(remainig / 3600) % 24,
+		remainigMin: Math.floor(remainig / 60) % 60,
+		remainigSec: Math.floor(remainig) % 60
+	}
+	
+	const documentObj = {
+		days: document.getElementById('days'),
+		hours: document.getElementById('hours'),
+		min: document.getElementById("min"),
+		sec: document.getElementById("sec")
+	}
+
+	// 반복문으로 변경하여 가독성 ++
+	
+	const timeKeys = Object.keys(remainigObj);
+	const docKeys = Object.keys(documentObj);
+
+	for (let i = 0; i < timeKeys.length; i = i + 1) {
+		documentObj[docKeys[i]].textContent = remainigObj[timeKeys[i]];
+	}
+
+	// documentObj['days'].textContent = remainigObj['remainigDate'];
+	// documentObj['hours'].textContent = remainigObj['remainigHours'];
+	// documentObj['min'].textContent = remainigObj['remainigMin'];
+	// documentObj['sec'].textContent = remainigObj['remainigSec'];
+
+  };
